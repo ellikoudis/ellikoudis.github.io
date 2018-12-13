36f18b1d278afa8bf9bff68aca71fbf94aeec1fb
Elena <ellikoudis@gmail.com>
2018-12-13 13:18:58 -0500

    #section-next

---
+++ js/#section-next.js
@@ -0,0 +1,27 @@
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+$(document).ready(function(){
+  $(".ct-btn-scroll").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  setInterval(function(){
+    $('.ct-btn-scroll').toggleClass("bounce");
+  }, 5000);
+
+});
